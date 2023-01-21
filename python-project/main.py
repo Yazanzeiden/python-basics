cc84a66b988135b4ae10e5969291ecd2fb443d01
yazan <yazanzeidany95@gmail.com>
2023-01-21 18:54:56 +0300

    add try, catch and while loop to the code

--- python-project/main.py
+++ python-project/main.py
@@ -1,7 +1,27 @@
-calculation_to_units = 24 * 60 * 60
-name_of_units = "seconds"
+calculation_to_hours = 24
+name_of_units = "hours"
 
-print(f"20 days are {20 * calculation_to_units} {name_of_units}")
-print(f"20 days are {35 * calculation_to_units } {name_of_units}")
-print(f"20 days are {50 * calculation_to_units} {name_of_units}")
-print(f"20 days are {110 * calculation_to_units} {name_of_units}")
+
+def days_to_units(num_of_days):
+    return f"{num_of_days} days are {num_of_days * calculation_to_hours} {name_of_units}"
+
+
+def validate_and_execute():
+    try:
+        user_input_number = int(user_input)
+        if user_input_number > 0:
+            calculated_value = days_to_units(user_input_number)
+            print(calculated_value)
+        elif user_input_number == 0:
+            print("you entered a 0, please enter a valid positive number")
+        else:
+            print("you entered a negative number, no conversion for you!")
+    except ValueError:
+        print("your input is not a valid number dont ruin my program")
+
+
+user_input = ""
+while user_input != "exit":
+    user_input = input("Hey user, enter a number of days and I will convert it to hours!\n")
+    validate_and_execute()
+
