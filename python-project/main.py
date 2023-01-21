cdf2cf718a39b266dad9012d1f6f2ef3c5ae9832
yazan <yazanzeidany95@gmail.com>
2023-01-21 16:42:58 +0300

    first-python-simple-programm

---
+++ python-project/main.py
@@ -0,0 +1,7 @@
+calculation_to_units = 24 * 60 * 60
+name_of_units = "seconds"
+
+print(f"20 days are {20 * calculation_to_units} {name_of_units}")
+print(f"20 days are {35 * calculation_to_units } {name_of_units}")
+print(f"20 days are {50 * calculation_to_units} {name_of_units}")
+print(f"20 days are {110 * calculation_to_units} {name_of_units}")
